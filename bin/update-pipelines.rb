38898dd82d52decd2c4dcd358817c49a73461c5d
Olivier ORAND <olivier.orand@orange.com>
2016-06-29 11:05:17 +0200

    initial revision

---
+++ bin/update-pipelines.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+require 'yaml'
+
+flyrc  = YAML.load_file(File.expand_path('~/.flyrc'))
+target_name="concourse-cw"
+target = flyrc['targets'][target_name]
+
+def header(msg)
+  print '*' * 10
+  puts " #{msg}"
+end
+
+def set_pipeline(target_name:,name:, cmd:, load: [])
+  puts system(%{bash -c "fly -t #{target_name} set-pipeline \
+    -p #{name} \
+    -c <(#{cmd}) \
+    -l private.yml
+  "})
+end
+
+header('For pipelines')
+Dir['pipelines/*.yml'].each do |filename|
+  name = File.basename(filename, '.yml')
+  puts "   #{name} pipeline"
+  set_pipeline(target_name: target_name,name: name, cmd: "erb #{filename}")
+end
+
+puts 'Done'
